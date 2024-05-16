7a69d54356883d61f52afcb234ce32e7ea0c9df0
1905guru <1905@gmail.com>
2024-05-16 10:40:45 +0530

    index changes

--- project12-crypto/client/vite-project/src/components/index.jsx
+++ project12-crypto/client/vite-project/src/components/index.jsx
@@ -2,5 +2,5 @@ export {default as Loader } from './Loader';
 export {default as  Navbar } from './Navbar';
 export {default as Footer} from './footer';
 export {default as Welcome } from './welcome';
-export {default as Transactions} from './transactions';
+export {default as Transactions} from './Transactions';
 export {default as Service } from './service';
